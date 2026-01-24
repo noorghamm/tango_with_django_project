3289a19547107e002412d16e44d0b7a97c4eb7ea
noorghamm <nooralghamm@gmail.com>
2026-01-24 16:35:45 +0000

    git status

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hi!")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page")
